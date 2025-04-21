bb11137e74f78d62bd3dcbb1900ab592284296d0
johanhyup <choup5525@gmail.com>
2025-04-22 02:05:04 +0900

    Update orders.php

--- public/api/orders.php
+++ public/api/orders.php
@@ -3,8 +3,8 @@
 
 header('Content-Type: application/json; charset=utf-8');
 require __DIR__ . '/../lib/BithumbAPI.php';
-define('ACCESS_KEY', '9b75dcaf4a9bda06be676d17fbca1fcc7aac22b676dbe8');
-define('SECRET_KEY', 'YTQxN2I4ZDA4MTZmODkwMmRhYWI0ZjlkZGEwMGNlMTc2YjE0ZmJjYjFmY2M3ZWFmMjBiMjc2ZDdlMDhjMA==');
+define('ACCESS_KEY', 'real key');
+define('SECRET_KEY', 'real key');
 
 // 디버그 로그
 file_put_contents(__DIR__.'/order_debug.log', print_r([
