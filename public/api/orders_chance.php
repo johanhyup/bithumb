ec5d008b02c62510530ac90532b52b12c401db87
johanhyup <choup5525@gmail.com>
2025-04-22 02:05:27 +0900

    Update orders_chance.php

--- public/api/orders_chance.php
+++ public/api/orders_chance.php
@@ -4,8 +4,8 @@
 header('Content-Type: application/json; charset=utf-8');
 require __DIR__ . '/../lib/BithumbAPI.php';
 
-define('ACCESS_KEY', '9b75dcaf4a9bda06be676d17fbca1fcc7aac22b676dbe8');
-define('SECRET_KEY', 'YTQxN2I4ZDA4MTZmODkwMmRhYWI0ZjlkZGEwMGNlMTc2YjE0ZmJjYjFmY2M3ZWFmMjBiMjc2ZDdlMDhjMA==');
+define('ACCESS_KEY', 'real key');
+define('SECRET_KEY', 'real key');
 
 $market = isset($_GET['market']) ? trim($_GET['market']) : null;
 if (!$market) {
